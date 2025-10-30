7f8017f5b060ac7400d318dcc0afc6ca159ee8c1
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2025-10-30 19:52:08 -0400

    refactor(BuildArtifacts): remove unused dfa_alphabet field

--- crates/regviz_core/src/core/mod.rs
+++ crates/regviz_core/src/core/mod.rs
@@ -24,8 +24,6 @@ pub struct BuildArtifacts {
     pub dfa: Option<Dfa>,
     /// A lazily computed minimal DFA.
     pub min_dfa: Option<Dfa>,
-    /// Alphabet corresponding to the DFA transition table when available.
-    pub dfa_alphabet: Option<Vec<char>>,
 }
 
 impl BuildArtifacts {
@@ -38,7 +36,6 @@ impl BuildArtifacts {
             alphabet,
             dfa: None,
             min_dfa: None,
-            dfa_alphabet: None,
         }
     }
 }
